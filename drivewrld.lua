b06297ab7fb693564d5bd272d618656527354434
uvul2 <160550857+uvul2@users.noreply.github.com>
2024-02-20 02:39:57 +0000

    Update drivewrld.lua

--- drivewrld.lua
+++ drivewrld.lua
@@ -153,6 +153,7 @@ while getfenv().test2 do
       local num = math.random(1,9)
   for i,v in pairs(game:GetService("Workspace").Jobs.TrailerDelivery.StartPoints:GetChildren()) do
      if i == num then
+  task.wait(5000)
   game:GetService("ReplicatedStorage").Systems.Jobs.StartJob:InvokeServer("TrailerDelivery", "6")
   repeat  wait()
      print("waiting for trailer")
