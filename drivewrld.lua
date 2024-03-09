45afbab8f222e93b514a0bf86834c8f73cc79ebe
uvul2 <160550857+uvul2@users.noreply.github.com>
2024-03-09 12:04:44 +0000

    Update drivewrld.lua

--- drivewrld.lua
+++ drivewrld.lua
@@ -12,9 +12,8 @@ local example = library:CreateWindow({
 
 example:AddToggle("Auto Kill", function(state)
    getfenv().kill = (state and true or false)
-while getfenv().ill do
+while getfenv().kill do
   game:GetService("ReplicatedStorage").Remotes.M1:FireServer(game:GetService("Players")["9037853282590723450S"].Character.Head, 5, false)
-end
 end)
 
 example:AddToggle("Auto Farm [Drift]", function(state)
